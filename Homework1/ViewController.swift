7b05b5be6f0905274dee0504d91d3fb2ecd4457c
Cheung TinLong <sky971016@gmail.com>
2018-04-10 13:14:13 +0800

    Initial Commit

---
+++ Homework1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homework1
+//
+//  Created by CheungTin Long on 8/4/2018.
+//  Copyright © 2018年 CheungTin Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
